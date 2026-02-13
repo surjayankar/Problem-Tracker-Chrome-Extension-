4f2c6f6ad9869328802642fd9986803f910fcb5e
Surjayan Kar <surjayan.smartboy@gmail.com>
2026-02-13 22:52:30 +0530

    Stored data in Chrome storage

--- content.js
+++ content.js
@@ -1,4 +1,5 @@
 const bookmarkImgUrl=chrome.runtime.getURL("assets/bookmark.png");
+const problemkey="problemkey"
 window.addEventListener("load",addBookMarkButton);
 function addBookMarkButton(){
     const bookmarkButton=document.createElement("button");
@@ -8,5 +9,37 @@ function addBookMarkButton(){
     bookmarkButton.style.height="24px";
     const askDoubtButton=document.getElementsByClassName("coding_ask_doubt_button__FjwXJ")[0];
     askDoubtButton.parentNode.insertAdjacentElement("afterend",bookmarkButton);
-    bookmarkButton.addEventListener("click");
+    bookmarkButton.addEventListener("click",addNewBookmarkHandler);
+}
+async function addNewBookmarkHandler(){
+    const currentBookmarks=await getCurrentBookmarks();
+    const currentUrl=window.location.href;
+    const uniqueId=extractUniqueId(currentUrl);
+    const problemname=document.getElementsByClassName("Header_resource_heading__cpRp1")[0].innerText;
+    if(currentBookmarks.some(bookmark=>bookmark.id===uniqueId)){
+        return;
+    }
+    const bookmarkObj={
+        id:uniqueId,
+        name:problemname,
+        url:currentUrl
+    }
+    const updatedBookmarks=[...currentBookmarks,bookmarkObj];
+    chrome.storage.sync.set({[problemkey]:updatedBookmarks},()=>{
+        console.log("Bookmark added successfully", updatedBookmarks);
+    })
+}
+function extractUniqueId(url){
+    const start=url.indexOf("problems/")+"problems/".length;
+    const end=url.indexOf("?",start);
+    return end===-1?url.substring(start):url.substring(start,end);
+}
+
+function getCurrentBookmarks(){
+
+    return new Promise((resolve,reject)=>{
+        chrome.storage.sync.get([problemkey],(results)=>{
+            resolve(results[problemkey]||[]);
+        });
+    })
 }
